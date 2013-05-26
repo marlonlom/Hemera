684fe1c2e7499fc1ceada44b13d901ebf7896ac0
Marlonlom <malm.gm@gmail.com>
2013-05-26 16:05:06 -0500

    javascript update

--- libs/dvp.app.js
+++ libs/dvp.app.js
@@ -2,11 +2,10 @@ var dvp = {};
 dvp.prepareMainView = function () {
         var title='titulo';
         var message='mensaje';
-        
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
         } else {
-            alert(title ? (title + ": " + message) : message);
+            alert(title + ": " + message);
         }
     }
 };
