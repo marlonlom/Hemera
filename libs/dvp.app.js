6849769990387da169e56f96cfc7b16db988041f
Marlonlom <malm.gm@gmail.com>
2013-05-26 16:15:20 -0500

    javascript update

--- libs/dvp.app.js
+++ libs/dvp.app.js
@@ -1,11 +1,5 @@
 var dvp = {};
 dvp.prepareMainView = function () {
-        var title='titulo';
-        var message='mensaje';
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title + ": " + message);
-        }
+        navigator.notification.alert("PhoneGap is working!!");
     }
 };
