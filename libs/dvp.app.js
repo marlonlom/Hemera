d6f569562b8c77f0ad32b95c599d1d93f32f675a
Marlonlom <malm.gm@gmail.com>
2013-06-02 16:46:11 -0500

    html css and js update

--- libs/dvp.app.js
+++ libs/dvp.app.js
@@ -1,5 +1,5 @@
 var dvp = {};
 dvp.prepareMainView = function () {
-        navigator.notification.alert("PhoneGap is working!!");
-    }
+    var mainMenuOptions = ['Departamentos', '&Aacute;reas Metropolitanas', 'Distritos', 'Datos de interes', 'Cont&aacute;ctenos'];
+    navigator.notification.alert("PhoneGap is working!!");
 };
