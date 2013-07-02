2181f5df7d0f85368794eff46989113787aca7a4
Marlonlom <malm.gm@gmail.com>
2013-07-01 20:38:16 -0500

    javascript update
    config fix

--- data/dvpm.page.evolution.js
+++ data/dvpm.page.evolution.js
@@ -1,66 +1,61 @@
 var dvpEvolutionContext = {
-	upperTip : "Glosario de términos",
-	listing : [{
-			title : "División político-administrativa de Colombia 2012",
-			leafs : [{
-					title : "División Político-administrativa de Colombia 2012",
-					url : "/data/xls/Listado_2012.xls"
-				}
-			]
-		}, {
-			title : "Consolidado Histórico Listado DIVIPOLA 2008 - 2011",
-			leafs : [{
-					title : "Listado División Político-administrativa de Colombia 2008",
-					url : "/data/xls/Listado_2008.xls"
-				}, {
-					title : "Listado División Político-administrativa de Colombia 2009",
-					url : "/data/xls/Listado_2009.xls"
-				}, {
-					title : "Listado División Político-administrativa de Colombia 2010",
-					url : "/data/xls/Listado_2010.xls"
-				}, {
-					title : "Listado División Político-administrativa de Colombia 2011",
-					url : "/data/xls/Listado_2011.xls"
-				}
-			]
-		}, {
-			title : "Consolidado Historico Resumen DIVIPOLA 1967 - 2012",
-			leafs : [{
-					title : "Resumen División Político-administrativa de Colombia 1967",
-					url : "/data/xls/Resumen_1967.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 1970",
-					url : "/data/xls/Resumen_1970.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2000",
-					url : "/data/xls/Resumen_2000.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2002",
-					url : "/data/xls/Resumen_2002.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2008",
-					url : "/data/xls/Resumen_2008.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2009",
-					url : "/data/xls/Resumen_2009.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2010",
-					url : "/data/xls/Resumen_2010.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2011",
-					url : "/data/xls/Resumen_2011.xls"
-				}, {
-					title : "Resumen División Político-administrativa de Colombia 2012",
-					url : "/data/xls/Resumen_2012.xls"
-				}
-			]
-		}, {
-			title : "Municipios creados desde 1993",
-			leafs : [{
-					title : "Resumen municipios creados desde 1993",
-					url : "/data/xls/Nuevos_municipios_desde_1993.xls"
-				}
-			]
-		}
-	]
+    upperTip: "Glosario de términos",
+    listing: [{
+        title: "División político-administrativa de Colombia 2012",
+        leafs: [{
+            title: "División Político-administrativa de Colombia 2012",
+            url: "http://localhost:8080/divipola-mobile-xls/about/listado/2012"
+        }]
+    }, {
+        title: "Consolidado Histórico Listado DIVIPOLA 2008 - 2011",
+        leafs: [{
+            title: "Listado División Político-administrativa de Colombia 2008",
+            url: "http://localhost:8080/divipola-mobile-xls/about/listado/2008"
+        }, {
+            title: "Listado División Político-administrativa de Colombia 2009",
+            url: "http://localhost:8080/divipola-mobile-xls/about/listado/2009"
+        }, {
+            title: "Listado División Político-administrativa de Colombia 2010",
+            url: "http://localhost:8080/divipola-mobile-xls/about/listado/2010"
+        }, {
+            title: "Listado División Político-administrativa de Colombia 2011",
+            url: "http://localhost:8080/divipola-mobile-xls/about/listado/2011"
+        }]
+    }, {
+        title: "Consolidado Historico Resumen DIVIPOLA 1967 - 2012",
+        leafs: [{
+            title: "Resumen División Político-administrativa de Colombia 1967",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/1967"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 1970",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/1970"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2000",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2000"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2002",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2002"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2008",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2008"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2009",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2009"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2010",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2010"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2011",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2011"
+        }, {
+            title: "Resumen División Político-administrativa de Colombia 2012",
+            url: "http://localhost:8080/divipola-mobile-xls/about/resumen/2012"
+        }]
+    }, {
+        title: "Municipios creados desde 1993",
+        leafs: [{
+            title: "Resumen municipios creados desde 1993",
+            url: "http://localhost:8080/divipola-mobile-xls/about/nuevos/1993"
+        }]
+    }]
 };
